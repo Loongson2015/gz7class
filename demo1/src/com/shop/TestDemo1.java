3924582bf7185fd2dfbfc57de8f8df2c2b427a8a
李四 <66666@qq.com>
2019-11-12 10:34:02 +0800

    这是我第一次使用idea提交代码。。。。

---
+++ demo1/src/com/shop/TestDemo1.java
@@ -0,0 +1,8 @@
+package com.shop;
+
+public class TestDemo1 {
+    public static void main(String[] args) {
+        System.out.println("这是我是每一次使用idea提交代码.....");
+        System.out.println("help git。。。。。");
+    }
+}
